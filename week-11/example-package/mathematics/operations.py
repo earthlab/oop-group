e5aedc5f544d5add8c9cd3ee36349e001b790e47
Max Joseph <maxwellbjoseph@gmail.com>
2019-04-04 14:03:48 -0600

    Adding notes on testing and example package skeleton

---
+++ week-11/example-package/mathematics/operations.py
@@ -0,0 +1,11 @@
+""" My great add module. """
+
+
+def add(x, y):
+    """ Add two numbers together. """
+    return x + y
+
+
+def multiply(x, y):
+    """ Multiply x and y. """
+    return x * y
---
+++ week-11/example-package/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name='mathematics',
+                 version='0.0.9999',
+                 description='The best mathematics package',
+                 url='https://bit.ly/IqT6zt',
+                 author='Max Joseph',
+                 author_email='maxwell.b.joseph@colorado.edu',
+                 license='MIT',
+                 packages=setuptools.find_packages())
